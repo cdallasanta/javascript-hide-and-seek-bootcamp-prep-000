d635c36a38ff0328d86a53173369fb2e6022dbdd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-24 19:20:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  let nest = document.querySelector("#nested");
+  return nest.querySelector(".target");
+}
+
+function increaseRankBy(n){
+  let li = document.querySelectorAll('.ranked-list li');
+  
+  for(var i =0; i<li.length;i++){
+    li[i].innerHTML = parseInt(li[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var deepDiv = document.querySelector("div#grand-node");
+  while(deepDiv.childElementCount !== 0){
+    deepDiv = deepDiv.querySelector('div');
+  }
+  return deepDiv;
+}
