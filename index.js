f49dd9faf38d8284f16becb7218c376edf796de8
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-24 19:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(nested, target){
+  debugger;
+  let nest = document.querySelector(nested);
+  return nest.querySelector(target);
+}
+
+function increaseRankBy(n){
+  let li = document.querySelectorAll('.ranked-list li');
+  
+  for(var i =0; i<li.length;i++){
+    li[i].innerHTML = parseInt(li[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var deepDiv = document.querySelector("div#grand-node");
+  while(deepDiv.childElementCount !== 0){
+    deepDiv = deepDiv.querySelector('div');
+  }
+  return deepDiv;
+}
